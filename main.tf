ba1a3c66bdf9aec6a6b28e78009032532d356600
ANawle <149271449+ANawle@users.noreply.github.com>
2025-03-18 15:48:53 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "~> 1.0"
+    }
+  }
+}
+
+provider "spacelift" {
+  # Spacelift API credentials (configured via environment variables)
+}
+
+# Fetch AWS Account ID from input
+variable "aws_account_id" {
+  description = "AWS Account ID for integration"
+  type        = string
+}
+
+# Fetch Stack Name (used as stack_id)
+variable "stack_name" {
+  description = "Spacelift Stack Name"
+  type        = string
+}
+
+# Create an AWS integration in Spacelift
+resource "spacelift_aws_integration" "aws_integration" {
+  name     = "aws-integration-${var.aws_account_id}"
+  role_arn = "arn:aws:iam::${var.aws_account_id}:role/Spacelift"
+  generate_credentials_in_worker = false
+}
