01c583062ec622846b3364786adf53145e5f7193
ANawle <149271449+ANawle@users.noreply.github.com>
2025-03-18 16:47:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,15 +34,27 @@ variable "stack_name" {
 # Create an AWS integration in Spacelift
 resource "spacelift_aws_integration" "aws_integration" {
   name                           = "aws-integration-${var.aws_account_id}"
-  role_arn                       = aws_iam_role.spacelift_role.arn
+  role_arn                       = data.aws_iam_role.spacelift_role.arn
   generate_credentials_in_worker = false
 }
 
-# Create or update the IAM Role for Spacelift
-resource "aws_iam_role" "spacelift_role" {
+# Fetch the existing IAM Role
+data "aws_iam_role" "spacelift_role" {
   name = "Spacelift"
+}
 
-  assume_role_policy = jsonencode({
+# Update the Assume Role Policy of the existing IAM Role
+resource "aws_iam_role_policy_attachment" "spacelift_trust_policy" {
+  role       = data.aws_iam_role.spacelift_role.name
+  policy_arn = aws_iam_policy.spacelift_assume_role_policy.arn
+}
+
+# Define the IAM Policy for Spacelift to assume the role
+resource "aws_iam_policy" "spacelift_assume_role_policy" {
+  name        = "SpaceliftAssumeRolePolicy"
+  description = "Policy allowing Spacelift to assume the role"
+
+  policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
